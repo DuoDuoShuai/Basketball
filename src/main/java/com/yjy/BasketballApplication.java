4c3faefd05062475d46cdd92e91e478b0143a373
哆哆 <965753827@qq.com>
2022-08-26 17:52:24 +0800

    第一次

---
+++ src/main/java/com/yjy/BasketballApplication.java
@@ -0,0 +1,37 @@
+package com.yjy;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@MapperScan("com.yiy.mapper")
+public class BasketballApplication {
+
+    private final static Logger logger = LoggerFactory.getLogger(BasketballApplication.class);
+
+    public static void main(String[] args) {
+        System.setProperty("druid.mysql.usePingMethod", "false");
+        ConfigurableApplicationContext run = SpringApplication.run(BasketballApplication.class, args);
+        Environment bean = run.getBean(Environment.class);
+        String port = bean.getProperty("server.port");
+        InetAddress addr;
+        try {
+            addr = InetAddress.getLocalHost();
+            logger.info("\n" +
+                    " :: 本地访问地址 : http://localhost:" + port + "\n" +
+                    " :: 局域网访问地址 : http://"+ addr.getHostAddress() + ":" + port + "\n" +
+                    " :: 数据监控地址 : http://"+ addr.getHostAddress() + ":" + port + "/druid\n" +
+                    " :: 公网访问地址 : 暂无");
+        } catch (UnknownHostException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
