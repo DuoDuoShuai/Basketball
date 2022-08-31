f3325e9a2c1ae3fd2c9319e1f32a9357e9fbf0bf
哆哆 <965753827@qq.com>
2022-08-31 15:56:05 +0800

    课程添加2

--- src/main/java/com/yjy/controller/CourseController.java
+++ src/main/java/com/yjy/controller/CourseController.java
@@ -6,13 +6,17 @@ import com.yjy.service.CourseService;
 import com.yjy.vo.JsonResult;
 import com.yjy.vo.JsonPageResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author 哆哆
@@ -40,17 +44,22 @@ public class CourseController {
 
    @RequestMapping("insertCourse")
    @ResponseBody
-    public JsonResult insertCourse(Course course, MultipartFile img) throws IOException {
+    public JsonResult insertCourse(Course course,MultipartFile img) throws IOException {
+       JsonResult success=null;
        if(img !=null){
            //设置图片路劲
            String originalFilename = img.getOriginalFilename();
-           String filepath="E:\\IDEA\\basketball\\src\\main\\resources\\static\\upload\\"+originalFilename;
+           String filepath="E:\\IDEA\\basketball\\src\\main\\resources\\static\\images\\"+originalFilename;
            //转存
            img.transferTo(new File(filepath));
            course.setPhoto(originalFilename);
+           course.setCourseId(UUID.randomUUID().toString());
+           Date currentTime=new Date(System.currentTimeMillis());
+           course.setUpdateTime(currentTime.getTime());
+           course.setCreateTime(currentTime.getTime());
+           Integer integer = courseService.insertCourse(course);
+           success = JsonResult.success(integer);
        }
-       int i = courseService.insertCourse(course);
-       JsonResult success = JsonResult.success(i);
        return success;
    }
 }
