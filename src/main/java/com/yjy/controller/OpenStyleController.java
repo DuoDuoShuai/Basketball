3f7aefa4367a262cc4ee5fbe856f4aca1c8d8934
ricardo <2315891407@qq.com>
2022-09-08 15:18:49 +0800

    完成公共信息风采CRUD

--- src/main/java/com/yjy/controller/OpenStyleController.java
+++ src/main/java/com/yjy/controller/OpenStyleController.java
@@ -1,32 +1,91 @@
 package com.yjy.controller;
 
 import com.yjy.dto.LayUiDto;
+import com.yjy.model.OpenRotation;
+import com.yjy.model.OpenStyle;
 import com.yjy.service.OpenStyleService;
 import com.yjy.vo.JsonPageResult;
+import com.yjy.vo.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * @author 任凭
  * @create 2022/8/30 15:37
  * @describe TODO
  */
-@Controller
-@RequestMapping("openStyle")
+@RestController
+@RequestMapping("OpenStyle")
 public class OpenStyleController {
 
     @Autowired
-    private OpenStyleService service;
+    private OpenStyleService openStyleService;
 
     /**
-     * 列表
+     * 公共风采数据全查询列表
+     *
      * @param dto
+     * @return map
+     */
+    @RequestMapping("list")
+    public JsonPageResult listOpenStyle(LayUiDto dto) {
+        JsonPageResult map = openStyleService.listOpenStyle(dto);
+        return map;
+    }
+
+    /**
+     * 新增公共风采
+     * @param openStyle
+     * @param img
+     * @param movice
      * @return
      */
-    @RequestMapping("listOpenStyle")
-    public JsonPageResult listOpenStyle(LayUiDto dto){
-        JsonPageResult map = service.listOpenStyle(dto);
-        return map;
+    @RequestMapping("insert")
+    public JsonResult insertOpenStyle(OpenStyle openStyle, MultipartFile img, MultipartFile movice) {
+        Integer insert = openStyleService.insertOpenStyle(openStyle, img,movice);
+        return JsonResult.success(insert);
+    }
+
+    /**
+     * 批量删除轮播图数据
+     *
+     * @param str
+     * @return
+     */
+    @RequestMapping("deleteMore")
+    public JsonResult deleteOpenStyleMore(String str) {
+        Integer deleteMore = openStyleService.deleteMore(str);
+        return JsonResult.success(deleteMore);
+    }
+
+    /**
+     * 删除单条风采数据
+     *
+     * @param styleId
+     * @return
+     */
+    @RequestMapping("delete")
+    public JsonResult deleteOpenStyle(String styleId) {
+        Integer delete = openStyleService.deleteOpenStyle(styleId);
+        return JsonResult.success(delete);
+    }
+
+    /**
+     * 编辑公共风采
+     * @param openStyle
+     * @param img
+     * @param video
+     * @return
+     */
+    @RequestMapping("update")
+    public JsonResult updateOpenStyle(OpenStyle openStyle, MultipartFile img,MultipartFile video) {
+        Integer update = openStyleService.updateOpenStyle(openStyle, img,video);
+        return JsonResult.success(update);
     }
 }
--- src/main/java/com/yjy/service/OpenStyleService.java
+++ src/main/java/com/yjy/service/OpenStyleService.java
@@ -1,13 +1,19 @@
 package com.yjy.service;
 
 import com.yjy.dto.LayUiDto;
+import com.yjy.model.OpenRotation;
+import com.yjy.model.OpenStyle;
+import com.yjy.model.Style;
 import com.yjy.vo.JsonPageResult;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author 任凭
  * @create 2022/8/30 14:30
  * @describe TODO
  */
+@Repository
 public interface OpenStyleService {
     /**
      * 列表
@@ -15,4 +21,45 @@ public interface OpenStyleService {
      * @return 页面表格数据
      */
     JsonPageResult listOpenStyle(LayUiDto dto);
+    /**
+     * 获取单个OpenStyle对象，根据Id查询
+     *
+     * @param styleId
+     * @return 一个公共风采
+     */
+    OpenStyle loadOpenStyle(String styleId);
+
+    /**
+     * 增加
+     * @param openStyle
+     * @param img
+     * @param video
+     * @return
+     */
+    Integer insertOpenStyle(OpenStyle openStyle, MultipartFile img, MultipartFile video);
+
+    /**
+     * 删除
+     *
+     * @param styleId
+     * @return 整数i=1
+     */
+    Integer deleteOpenStyle(String styleId);
+
+    /**
+     * 批量删除
+     *
+     * @param openStyleIds
+     * @return
+     */
+    Integer deleteMore(String openStyleIds);
+
+    /**
+     * 修改
+     * @param openStyle
+     * @param img
+     * @param video
+     * @return
+     */
+    Integer updateOpenStyle(OpenStyle openStyle, MultipartFile img,MultipartFile video);
 }
