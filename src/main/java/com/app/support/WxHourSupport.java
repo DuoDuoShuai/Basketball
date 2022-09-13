52603bca3fe7243e0876f29e9b871d5d9863fac5
哆哆 <965753827@qq.com>
2022-09-13 13:09:09 +0800

    1、教练端销课

---
+++ src/main/java/com/app/support/WxHourSupport.java
@@ -0,0 +1,26 @@
+package com.app.support;
+
+import com.app.mapper.WxHourMapper;
+import com.yjy.model.Hour;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author 哆哆
+ * @description
+ * @date 2022-09-09 10:14:12
+ * @describe: TODO
+ */
+public class WxHourSupport {
+    @Autowired
+    private WxHourMapper wxHourMapper;
+
+    /**
+     * 接口--根据id查销课相关的信息--学员模块和班级模块
+     * @param hourId
+     * @return
+     */
+
+    public Hour hourById(String hourId){
+        return wxHourMapper.hourById(hourId);
+    }
+}
