e560cf0c1bb238bbce998b055d5596649b065837
米奇妙妙屋 <1579764068@qq.com>
2022-09-14 16:43:08 +0800

    小程序教练端接口支持完成

--- src/main/java/com/app/support/WxEnrollSupport.java
+++ src/main/java/com/app/support/WxEnrollSupport.java
@@ -1,12 +1,17 @@
 package com.app.support;
 
 import com.app.dto.WxAllDto;
+import com.app.dto.WxInsertDto;
 import com.app.mapper.WxEnrollMapper;
+import com.app.mapper.WxEnrollTypeMapper;
 import com.yjy.model.Enroll;
+import com.yjy.model.EnrollType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @Author:黄文倩
@@ -17,6 +22,10 @@ import java.util.List;
 public class WxEnrollSupport {
     @Autowired
     private WxEnrollMapper wxEnrollMapper;
+    @Autowired
+    private WxEnrollTypeMapper wxEnrollTypeMapper;
+    @Autowired
+    private WxStudentSupport wxStudentSupport;
 
     /**
      * 接口--查询学员不在班级学员表中的报名记录
@@ -26,4 +35,26 @@ public class WxEnrollSupport {
         return wxEnrollMapper.listByGrade(wxAllDto);
     }
 
+    /**
+     * 接口--添加新学员时生成报名记录
+     * @param wxInsertDto
+     * @return
+     */
+    public Integer insertEnroll(WxInsertDto wxInsertDto) {
+        String studentName = wxInsertDto.getStudentName();
+        EnrollType enrollType = new EnrollType();
+        enrollType.setStudentName(studentName);
+        wxEnrollTypeMapper.updateEnrollType(enrollType);
+        wxStudentSupport.insertByEnroll(wxInsertDto);
+
+        Enroll enroll = new Enroll();
+        enroll.setEnrollId(UUID.randomUUID().toString());
+        Date currentTime=new Date(System.currentTimeMillis());
+        enroll.setCreatTime(currentTime.getTime());
+        enroll.setCourseName(wxInsertDto.getCourseName());
+        enroll.setPayType(wxInsertDto.getPayType());
+        enroll.setSchoolNme(wxInsertDto.getSchoolName());
+        enroll.setStudentName(wxInsertDto.getStudentName());
+        return wxEnrollMapper.insertEnroll(enroll);
+    }
 }
