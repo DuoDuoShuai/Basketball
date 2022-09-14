d2839364ef92e9812c043de47e82c523a01b98f2
米奇妙妙屋 <1579764068@qq.com>
2022-09-14 10:17:15 +0800

    小程序教练端用户模块完成

--- src/main/java/com/app/controller/WxEnrollController.java
+++ src/main/java/com/app/controller/WxEnrollController.java
@@ -1,6 +1,7 @@
 package com.app.controller;
 
 import com.app.dto.WxAllDto;
+import com.app.support.WxCourseSupport;
 import com.app.support.WxEnrollSupport;
 import com.app.vo.WxJsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("wx_enroll")
 public class WxEnrollController {
+
     @Autowired
     private WxEnrollSupport wxEnrollSupport;
+    @Autowired
+    private WxCourseSupport wxCourseSupport;
+
+    /**
+     * 查询为未班学员
+     * @param wxAllDto
+     * @return
+     */
     @RequestMapping("listByGrade")
     public WxJsonResult listByGrade(WxAllDto wxAllDto){
         return WxJsonResult.success(wxEnrollSupport.listByGrade(wxAllDto));
+    }
+
+    @RequestMapping("loadCourse")
+    public WxJsonResult loadCourse(String courseId){
+        return WxJsonResult.success(wxCourseSupport.loadCourseById(courseId));
     }
 }
